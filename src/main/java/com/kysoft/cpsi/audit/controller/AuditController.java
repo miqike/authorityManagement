f26fc867bc840949f39c792d44f2f25f600ff0e4
YuQingHe <yuqh_wud@163.com>
2016-05-25 11:10:53 +0800

    存续状态核查

--- src/main/java/com/kysoft/cpsi/audit/controller/AuditController.java
+++ src/main/java/com/kysoft/cpsi/audit/controller/AuditController.java
@@ -1,60 +1,74 @@
 package com.kysoft.cpsi.audit.controller;
 
-import java.util.Map;
-
-import javax.annotation.Resource;
-
+import com.google.common.collect.Maps;
+import com.kysoft.cpsi.audit.entity.MailVerifyException;
+import com.kysoft.cpsi.audit.service.AuditService;
+import com.kysoft.cpsi.task.service.HcsxjgService;
+import net.sf.husky.web.controller.BaseController;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.google.common.collect.Maps;
-import com.kysoft.cpsi.audit.entity.MailVerifyException;
-import com.kysoft.cpsi.audit.service.AuditService;
-
-import net.sf.husky.web.controller.BaseController;
+import javax.annotation.Resource;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/audit")
 public class AuditController extends BaseController {
 
-	@Resource
-	AuditService auditService;
+    @Resource
+    AuditService auditService;
 
-	@RequestMapping(value = "/sentVerifyMail", method = RequestMethod.POST)
-	public Map<String, Object> sentVerifyMail(String hcrwId,
-			String hcsxId,
-			String mail) {
-		Map<String, Object> result = Maps.newHashMap();
-		try {
-			auditService.sentVerifyMail(hcrwId, hcsxId, mail);
-			result.put(MESSAGE, "验证邮件发送成功");
-			result.put(STATUS, SUCCESS);
-		} catch (MailVerifyException e) {
-			e.printStackTrace();
-			result.put(STATUS, FAIL);
-			result.put(MESSAGE, "验证邮件发送失败: " + e.getMessage());
-		}
-		return result;
-	}
-	
-	
-	@RequestMapping(value = "/mail/{id}", method = RequestMethod.GET)
-	public Map<String, Object> verifyMail(@PathVariable String id) {
-		Map<String, Object> result = Maps.newHashMap();
-		try {
-			auditService.verifyMail(id);
-			result.put(MESSAGE, "邮件验证成功");
-			result.put(STATUS, SUCCESS);
-		} catch (MailVerifyException e) {
-			e.printStackTrace();
-			result.put(STATUS, FAIL);
-			result.put(MESSAGE, "邮件验证失败: " + e.getMessage());
-		}
-		return result;
-	}
-	
-	
-	
+    @Resource
+    HcsxjgService hcsxjgService;
+
+    @RequestMapping(value = "/sentVerifyMail", method = RequestMethod.POST)
+    public Map<String, Object> sentVerifyMail(String hcrwId,
+                                              String hcsxId,
+                                              String mail) {
+        Map<String, Object> result = Maps.newHashMap();
+        try {
+            auditService.sentVerifyMail(hcrwId, hcsxId, mail);
+            result.put(MESSAGE, "验证邮件发送成功");
+            result.put(STATUS, SUCCESS);
+        } catch (MailVerifyException e) {
+            e.printStackTrace();
+            result.put(STATUS, FAIL);
+            result.put(MESSAGE, "验证邮件发送失败: " + e.getMessage());
+        }
+        return result;
+    }
+
+
+    @RequestMapping(value = "/mail/{id}", method = RequestMethod.GET)
+    public Map<String, Object> verifyMail(@PathVariable String id) {
+        Map<String, Object> result = Maps.newHashMap();
+        try {
+            auditService.verifyMail(id);
+            result.put(MESSAGE, "邮件验证成功");
+            result.put(STATUS, SUCCESS);
+        } catch (MailVerifyException e) {
+            e.printStackTrace();
+            result.put(STATUS, FAIL);
+            result.put(MESSAGE, "邮件验证失败: " + e.getMessage());
+        }
+        return result;
+    }
+
+    @RequestMapping(value = "/complete", method = RequestMethod.POST)
+    public Map<String, Object> complete(String hcrwId, String hcsxId, Integer hcjg, String qymc, String hcsxmc) {
+        Map<String, Object> result = Maps.newHashMap();
+        try {
+            hcsxjgService.complete(hcrwId, hcsxId, hcjg);
+            result.put(MESSAGE, "验证[" + qymc + "]的" + "[" + hcsxmc + "]完成");
+            result.put(STATUS, SUCCESS);
+        } catch (MailVerifyException e) {
+            e.printStackTrace();
+            result.put(STATUS, FAIL);
+            result.put(MESSAGE, "验证[" + qymc + "]的" + "[" + hcsxmc + "]失败");
+        }
+        return result;
+    }
+
 }
