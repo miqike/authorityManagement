5a9909f20105183b7df21b31fb1ab2265c8e6605
yqh <yuqh_wud@163.com>
2016-06-20 14:58:33 +0800

    修改接口相关

--- doc/cpsi_init_2.sql
+++ doc/cpsi_init_2.sql
@@ -40,13 +40,13 @@ begin
   insert into sys_organization(id,name,parent_id,type,contacts,phone,brief_name)
     select code id,content name,sjcode2 parent_id,1 type,null contacts,null phone,djjgjc brief_name  from(
       select a.*,case when sjcode='0' and code not in('100000','610000') then '610000' else sjcode end sjcode2
-      from bm_djjg@Dbl_Cpsi a where code not in('100000')
+      from bm_djjg a where code not in('100000')
       start with sjcode='0' connect by prior code=sjcode
     ) start with sjcode2='0' connect by prior code=sjcode2;
 
   for o in cur_org loop
     insert into sys_organization(id,name,parent_id,type,contacts,phone,brief_name)
-      select code id,content name,o.id parent_id,1 type,null contacts,null phone,null brief_name from BM_gxdw@Dbl_Cpsi where code like o.id||'%' and length(code)=8;
+      select code id,content name,o.id parent_id,1 type,null contacts,null phone,null brief_name from BM_gxdw where code like o.id||'%' and length(code)=8;
   end loop;
 end;
 /
@@ -56,9 +56,9 @@ end;
  */
 delete from t_zfry;
 insert into t_zfry(code,name,gender,dw_id,dw_name,zw,mobile,mail,zfzh,sfzh,zflx,whcd,zt)
-  select user_name,full_name,null gender,djjg dw_id,(select content from bm_djjg@dbl_cpsi b where b.code=a.djjg) dw_name,
+  select user_id,full_name,null gender,djjg dw_id,(select content from bm_djjg b where b.code=a.djjg) dw_name,
                              null zw,null mobile,null mail,zfzh,null sfzh,1,null whcd,1
-  from v_xt_user@dbl_cpsi a;
+  from xt_user a;
 /**
   初始化操作员
  */
@@ -67,14 +67,30 @@ delete from sys_user_role a where not exists(select 1 from sys_user b where b.us
 
 insert into sys_user(user_id,manager_id,name,email,mobile,password,salt,create_time,manager_name,status,org_id,org_type,weight,org_name,zfry)
   select gh user_id,null manager_id,full_name name,null email,null mobile,lower(MD5_DIGEST(gh||'000000'||'123qwe!@#QWE')) password,'123qwe!@#QWE' salt,sysdate create_time,
-         null manager_name,1 status,djjg org_id,0 org_type,1 weight,(select content from bm_djjg@dbl_cpsi b where b.code=a.djjg) org_name,
+         null manager_name,1 status,djjg org_id,0 org_type,1 weight,(select content from bm_djjg b where b.code=a.djjg) org_name,
          user_name zfry
-  from v_xt_user@dbl_cpsi a where gh is not null and gh not in('06020','06033','05035');
+  from xt_user a where gh is not null and gh not in(select gh from(select gh,count(1) from xt_user where gh is not null group by gh having count(1)>1));
 insert into sys_user_role(role_id,user_id)
   select a.id,b.user_id from sys_role a,sys_user b
   where a.name in('超级管理员')
         and b.user_id<>'system';
 update sys_user set password=lower(MD5_DIGEST(user_id||'111111'||salt));
+
+/**
+初始化编码表
+**/
+delete from x_codelist where name='ztlx';
+insert into x_codelist(name,value,literal,edit_flag,style,descn)
+  select 'ztlx',code,content,0,null,'企业主体类型' from bm_qydl;
+delete from x_codelist where name='jyzt';
+insert into x_codelist(name,value,literal,edit_flag,style,descn)
+  select 'jyzt',code,content,0,null,'经营状态' from BM_TCKYQK;
+delete from x_codelist where name='czxs';
+insert into x_codelist(name,value,literal,edit_flag,style,descn)
+  select 'czxs',code,content,0,null,'出资形式' from BM_CZXS;
+delete from x_codelist where name='wzlx';
+insert into x_codelist(name,value,literal,edit_flag,style,descn)
+  select 'wzlx',code,content,0,null,'网址类型' from BM_WZLX;
 /**
 开发测试过程中可能需要用到的SQL
  */
@@ -82,3 +98,24 @@ update sys_user set password=lower(MD5_DIGEST(user_id||'111111'||salt));
 select * from sys_user a where exists(select 1 from t_hcrw b where b.zfry_code1=a.zfry and not exists(select 1 from t_hcsxjg c where c.hcrw_id=b.id));
 --查询操作员，条件是此操作员有核查过的企业
 select * from sys_user a where exists(select 1 from t_hcrw b where b.zfry_code1=a.zfry and exists(select 1 from t_hcsxjg c where c.hcrw_id=b.id));
+
+/***
+20160620 修改表结构
+ */
+alter table cpsi.t_nb add yyzsr number(10,2);
+comment on column cpsi.t_nb.yyzsr is '营业总收入';
+alter table cpsi.t_nb add dj_dzzjz integer;
+alter table cpsi.t_nb add dj_frdbsfdzzsj integer;
+comment on column cpsi.t_nb.dj_dzzjz is '党组织建制';
+comment on column cpsi.t_nb.dj_frdbsfdzzsj is '法人代表是否为党组织书记';
+alter table cpsi.t_nb_dwtz add tzqy_zch varchar2(100);
+comment on column cpsi.t_nb_dwtz.tzqy_zch is '被投资企业注册号';
+
+alter table cpsi.t_nb_bd add yyzsr number(10,2);
+comment on column cpsi.t_nb_bd.yyzsr is '营业总收入';
+alter table cpsi.t_nb_bd add dj_dzzjz integer;
+alter table cpsi.t_nb_bd add dj_frdbsfdzzsj integer;
+comment on column cpsi.t_nb_bd.dj_dzzjz is '党组织建制';
+comment on column cpsi.t_nb_bd.dj_frdbsfdzzsj is '法人代表是否为党组织书记';
+alter table cpsi.t_nb_bd_dwtz add tzqy_zch varchar2(100);
+comment on column cpsi.t_nb_bd_dwtz.tzqy_zch is '被投资企业注册号';
