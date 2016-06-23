b46076e0670210d6e51c5bae5b83b1ba5a78ed46
Tommy <coralsea_li@yeah.net>
2016-06-23 09:13:09 +0800

    a

--- src/main/webapp/51/auditItemList.js
+++ src/main/webapp/51/auditItemList.js
@@ -220,9 +220,11 @@ function _doInit(type) {
     $("#_qymc_").text(qy.hcdwName);
     $("#_hcsxmc_").text(auditItem.name);
 
+    $("#btnOpenEtlTool").show();
     $("#btnSuccess").show();
     $("#btnFail").show();
 
+    $("#btnOpenEtlTool").click(openEtlTool);
     $("#btnSuccess").click(pass);
     $("#btnFail").click(fail);
 
@@ -259,6 +261,59 @@ function addComment(elem) {
 	var content = $(elem).text();
 	$("#k_failReason").text($("#k_failReason").text() + "\n" + content);
 }
+
+//=============================
+function openEtlTool() {
+	getUserInfo();
+    if (null != window.userInfo) {
+        execute();
+    } else {
+        $.subscribe("USERINFO_INITIALIZED", execute);
+    }
+}
+
+function executeFile(file, paramArray) {
+    try {
+        var _file = file;
+        var fso = new ActiveXObject("Scripting.FileSystemObject");
+        if (fso.FileExists(_file)) {
+            var shellActiveXObject = new ActiveXObject("WScript.Shell");
+            if (!shellActiveXObject) {
+                alert('无法创建WScript.Shell');
+                return;
+            }
+            var exePath = "";
+            $.getJSON("../user/" + userInfo.userId + "/all", null, function (response) {
+                var qy = $("#grid1").datagrid("getSelected");
+                //用户名&salt&加密后的密码&计划单编号&企业注册号&企业名称
+                exePath = file + " " + response.userId + "&" + response.salt + "&" + response.password + "&" + $("#p_id").val() + "&" + qy.hcdwXydm + "&" + qy.hcdwName;
+            });
+
+            if (null != paramArray) {
+                for (var i = 0; i < paramArray.length; i++) {
+                    exePath = exePath + " " + paramArray[i];
+                }
+                alert(exePath);
+            }
+
+            shellActiveXObject.Run(exePath, 1, false);
+            shellActiveXObject = null;
+        }
+        else {
+            alert("系统检测到未安装" + file);
+        }
+    }
+    catch (errorObject) {
+        alert("请将站点设置为可信任站点，并将其安全级别设置为低!" + errorObject.message);
+    }
+}
+//更新单位树
+function execute() {
+    executeFile("C:/ky1.0/sjp6.exe");
+}
+
+//=============================
+
 //==通用,通过,失败,返回===
 function pass() {
     $.post("../audit/complete", {
--- src/main/webapp/audit_61/finicial.jsp
+++ src/main/webapp/audit_61/finicial.jsp
@@ -8,15 +8,14 @@ var auditApproach =
 	"<p style='margin: 2px 2px 2px 142px;'> c.清算：dddd要求企业提交清算法定事由产生的文件，如股东会决议、清算组备案手续、企业注销公告、司法文书、政府文件等。</p>"
 	
 </script>
-<%@ include  file="singleAudit.jsp"%> 
  --%>
 
 <%@ page contentType="text/html; charset=UTF-8" %>
 <script>
 var auditApproach = 
 	"<p style='margin: 2px;'> a.核查财务数据</p>" +
-	"<p style='margin: 2px 2px 2px 62px;'> b.歇业：可参考提交的税务报停手续或其他资料。</p>" +
-	"<p style='margin: 2px 2px 2px 62px;'> c.清算：dddd要求企业提交清算法定事由产生的文件，如股东会决议、清算组备案手续、企业注销公告、司法文书、政府文件等。</p>"
+	"<p style='margin: 2px 2px 2px 70px;'> b.歇业：可参考提交的税务报停手续或其他资料。</p>" +
+	"<p style='margin: 2px 2px 2px 70px;'> c.清算：要求企业提交清算法定事由产生的文件，如股东会决议、清算组备案手续、企业注销公告、司法文书、政府文件等。</p>"
 
 var auditConfig = [
 	["syzqyhj", "所有者权益合计"], 
